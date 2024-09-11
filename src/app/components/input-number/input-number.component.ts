6f95aa1b6370c734111912a8227aa52062e9227b
mkcodelab <michal.kulawik@gmail.com>
2024-09-11 13:11:41 +0200

    input-number component upgrade, project-settings refactor

--- src/app/components/input-number/input-number.component.ts
+++ src/app/components/input-number/input-number.component.ts
@@ -1,36 +1,44 @@
 import { Component, Input } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'input-number',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './input-number.component.html',
   styleUrl: './input-number.component.scss',
 })
 export class InputNumberComponent {
-  @Input() value: number = 0;
+  private _value: number = 0;
   @Input() step: number = 1;
   @Input() min: number;
   @Input() max: number;
   @Input() label: string;
 
+  @Input() set value(value: number) {
+    this._value = value;
+  }
+  get value() {
+    return +this._value.toFixed(3);
+  }
+
   increment() {
     if (this.max) {
-      if (this.value < this.max) {
-        this.value += this.step;
+      if (this._value < this.max) {
+        this._value += this.step;
       }
     } else {
-      this.value += this.step;
+      this._value += this.step;
     }
   }
 
   decrement() {
     if (this.min !== undefined) {
-      if (this.value > this.min) {
-        this.value -= this.step;
+      if (this._value > this.min) {
+        this._value -= this.step;
       }
     } else {
-      this.value -= this.step;
+      this._value -= this.step;
     }
   }
 }
--- src/app/components/project-settings/project-settings.component.ts
+++ src/app/components/project-settings/project-settings.component.ts
@@ -1,10 +1,12 @@
 import { Component, inject } from '@angular/core';
 import { ProjectDataService } from '../../services/project-data.service';
+import { InputNumberComponent } from '../input-number/input-number.component';
 
 @Component({
   standalone: true,
   selector: 'project-settings',
   templateUrl: './project-settings.component.html',
+  imports: [InputNumberComponent],
 })
 export class ProjectSettingsComponent {
   projectDataSvc = inject(ProjectDataService);
